de230a3fc92a7153ab87758837ccdd484e481724
nicerobot <git@nicerobot.org>
2010-07-11 08:14:21 -0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## Crash
+
+Nothing but crash
+
+TODO
+
+- Provide a button that will crash the app.
+- Implementing a settings page to toggle between
+  - button-crasher
+  - startup-crasher
---
+++ Shared/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  Crash
+//
+//  Created by Robert Nix on 2010.11.07.
+//  Copyright nicerobot.org 2010. All rights reserved.
+//
+
+int main(int argc, char *argv[]) {  
+  printf(0);
+  return 1;
+}
